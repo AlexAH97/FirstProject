1da20f9fc850805169ef9ff031037e2a0df55789
ANISORAC ALEXANDRU HOREA <alexanisorac@yahoo.com>
2019-04-09 15:35:57 +0300

    First Commit

---
+++ Assignment1/Assignment1/BusinessLogic/FactoryMethod/AbstracClasst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1.BusinessLogic.FactoryMethod
+{
+    public abstract class AbstractClass
+    {
+        public abstract String getBy(Users user);
+    }
+
+
+}
---
+++ Assignment1/Assignment1/BusinessLogic/FactoryMethod/GetByGenre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1.BusinessLogic.FactoryMethod
+{
+    class GetByGenre:AbstractClass
+    {
+        public override string getBy(Users user)
+        {
+            return String.Empty;
+        }
+    }
+}
